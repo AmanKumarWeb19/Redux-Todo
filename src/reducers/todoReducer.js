842e145a171dedfb70d525bbaa3e60b1ebe60a17
Aman Kumar <akgupta0524@gmail.com>
2024-11-07 12:44:07 +0530

    delete todo and remove todo function

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -19,6 +19,18 @@ const todoReducer = (state = initialData, action) => {
           },
         ],
       };
+    case "DELETE_TODO":
+      const newList = state.list.filter((elem) => elem.id !== action.id);
+      return {
+        ...state,
+        list: newList,
+      };
+
+    case "REMOVE_TODO":
+      return {
+        ...state,
+        list: [],
+      };
 
     default:
       return state;
